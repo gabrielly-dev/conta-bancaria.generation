7a9113753fc23fb1cafaf747f8886c86f7e31002
Gabrielly Barbosa Silva <gabriellybarbosa.dev@gmail.com>
2025-02-12 19:55:44 -0300

    Adicionando a classe conta e implementandoalguns métodos

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,13 +1,20 @@
 package conta;
 
+import conta.model.Conta;
 import java.util.Scanner;
-
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
 		Scanner leitura = new Scanner(System.in);
 		
 		int opcao;
